b59e77875a89130190a250a69d87087fb2d614a7
Romiyus <104280247+Yusna21@users.noreply.github.com>
2023-06-10 18:02:04 +0000

    Add Structure & Stack implementation
    Membuat PasswordData untuk menyimpan nilai akun dan password, lalu implementasi stack sebagai function

--- main.cpp
+++ main.cpp
@@ -4,3 +4,67 @@
 using namespace std;
 
 const string VERSION = "1.0.0";
+
+// untuk menyimpan data
+struct PasswordData
+{
+    string akun;
+    string password;
+};
+
+// implementasi stack 
+class Stack 
+{
+    private:
+        static const int MAX_SIZE = 100;
+        PasswordData data[MAX_SIZE];
+        int top;
+    
+    public:
+        Stack() 
+        {
+            top = -1;
+        }
+
+        void push(PasswordData password)
+        {
+            if (top >= MAX_SIZE - 1) 
+            {
+                cout << "Data ditambahkan" << endl;
+                return;
+            }
+            data[++top] = password;
+        }
+
+        void pop()
+        {
+            if (isEmpty())
+            {
+                cout << "Stack kosong!" << endl;
+                return;
+            }
+            top--;
+        }
+
+        PasswordData topElement()
+        {
+            if (isEmpty())
+            {
+                PasswordData emptyData;
+                emptyData.akun = "";
+                emptyData.password = "";
+                return emptyData;
+            }
+            return data[top];
+        }
+
+        bool isEmpty()
+        {
+            return top == -1;
+        }
+};
+
+int main() 
+{
+    cout << "test";
+}
