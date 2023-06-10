79fb45922be7c9bd1273299b6b1c66db399703cb
Romiyus <104280247+Yusna21@users.noreply.github.com>
2023-06-10 18:15:59 +0000

    Add Queue algoritm functon

--- main.cpp
+++ main.cpp
@@ -64,7 +64,69 @@ class Stack
         }
 };
 
+class Queue 
+{
+    private:
+        static const int MAX_SIZE = 100;
+        PasswordData data[MAX_SIZE];
+        int depan;
+        int belakang;
+
+    public:
+        Queue()
+        {
+            depan = -1;
+            belakang = -1;
+        }
+
+        void enqueue(PasswordData passwordData)
+        {
+            if (belakang == MAX_SIZE - 1)
+            {
+                cout << "Queue Terisi" << endl;
+                return;
+            }
+
+            data[++belakang] = passwordData;
+            if (depan == - 1) 
+            {
+                depan = 0;
+            }
+        }
+
+        void dequeue()
+        {
+            if (isEmpty())
+            {
+                cout << "Queue kosong!" << endl;
+                return;
+            }
+            depan++;
+            if (depan > belakang)
+            {
+                depan = -1;
+                belakang = -1;
+            }
+        }
+
+        PasswordData elemenDepan()
+        {
+            if (isEmpty())
+            {
+                PasswordData emptyData;
+                emptyData.akun = "";
+                emptyData.password = "";
+                return emptyData;
+            }
+            return data[depan];
+        }
+        bool isEmpty()
+        {
+            return depan == -1;
+        }
+};
+
 int main() 
 {
-    cout << "test";
+    cout << "test queue";
 }
